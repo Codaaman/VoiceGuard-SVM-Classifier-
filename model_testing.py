53c5d94158af55bcc52dc095d9b4f306fc2e9629
Aman Kumar Bhardwaj <coderaman30@gmail.com>
2026-09-15 18:47:48 +0530

    Update model_testing.py

--- model_testing.py
+++ model_testing.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder,StandardScaler
+from sklearn.preprocessing import LabelEncode,StandardScaler
 import pandas as pd
 from sklearn.svm import SVC
 import joblib
@@ -35,20 +35,16 @@ while True:
    
     scaler=joblib.load('')
     
-    encoder=joblib.load('')
-    
     scaled_featurs=scaler.transform(scaled_mfccs)
-
     pred = model.predict(scaled_featurs)
-    result=encoder.inverse_transform(pred)
     print("Predicted class:", result)
     
-    if result[0]=='Anvesha':
+    if result[0]=='Aman':
         '''keyboard.press('ctrl+d')
         time.sleep(15)
         keyboard.press('ctrl+d')'''
 
-        print("Anvesha")
+        print("Aman")
 
         '''pygame.mixer.music.load('D:\\spotyfy_python\\ji.mp3')
         pygame.mixer.music.play()'''
